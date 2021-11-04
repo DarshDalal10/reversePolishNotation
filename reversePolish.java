d7b6def5631bed1c9e655421320b60f2fd57707a
Darsh Dalal <65700575+DarshDalal10@users.noreply.github.com>
2021-11-04 15:28:24 -0400

    Add initial files

---
+++ reversePolish.java
@@ -0,0 +1,22 @@
+class reversePolish {
+
+
+
+    public static void main(String[] args) {
+        String arithmeticString = args[0];
+
+        System.out.println(arithmeticString);
+
+        String[] terms = arithmeticString.split(" ");
+
+        System.out.println(arithmeticString);
+        
+        
+
+    }
+
+
+
+
+
+}
